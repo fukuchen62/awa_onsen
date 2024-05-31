87941da3dc97086f5f3b19737a845b822ff513c8
sawaka527 <shuangshanxia7@gmail.com>
2024-05-31 17:21:14 +0900

    0531
    更新
    
    山下

---
+++ assets/js/top.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// $(function () {
+//     $(".slider").slick({
+//         autoplay: true,
+//         dots: true,
+//         adaptiveHeight: true,
+//         prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+//         nextArrow: '<button class="slide-arrow next-arrow"></button>',
+//     });
+// });
+
+
+
+$(function () {
+    $(".slider").slick({
+        autoplay: true,
+        dots: true,
+        adaptiveHeight: true,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>'
+    });
+});
