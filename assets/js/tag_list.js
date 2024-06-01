935585aeb6f8e66c430b8caf0c421e19ab1eed8d
sakiiiina3208 <matsuo.web2@gmail.com>
2024-06-01 22:46:35 +0900

    お知らせ一覧
    お知らせ一覧のCSS、Javascriptを編集
    
    松尾

--- assets/js/tag_list.js
+++ assets/js/tag_list.js
@@ -1,40 +1,46 @@
 "use strict";
 
-// タグクリックでカテゴリー切り替え
-const newsTags = document.querySelectorAll('.news_tag, .gourmet_tag, .play_tag, .others_tag');
-const newsLists = document.querySelectorAll('.news_list');
+// ▼タグクリックでカテゴリー切り替え▼
+// info_tag内のli要素を取得して配列newsTagsに代入。
+let newsTags = document.querySelectorAll('.news_tag, .gourmet_tag, .play_tag, .others_tag');
+
+// news_listの要素を取得して変数newsListsに代入。
+let newsLists = document.querySelectorAll('.news_list');
 
 function showList(listClass) {
-    hideAllLists();
-    const targetList = document.querySelector(`.news_list.${listClass}`);
+    hideAllLists();  // すべてのnewsListsを非表示。
+    let targetList = document.querySelector(`.news_list.${listClass}`); // 対応するnews_listの要素を取得。
     if (targetList) {
-        targetList.classList.add('show');
+        targetList.classList.add('show');  // news_listに'show'クラスを追加し、表示。
     }
 }
 
 function hideAllLists() {
-    newsLists.forEach(list => list.classList.remove('show'));
+    newsLists.forEach(list => list.classList.remove('show'));  // すべてのnews_listから'show'クラスを削除し、非表示に。
 }
 
 function initializeActiveTag() {
-    const newsTag = document.querySelector('.news_tag');
+    let newsTag = document.querySelector('.news_tag');  // 初期値のタグ要素（news_tag）を取得
     if (newsTag) {
-        newsTag.classList.add('active');
+        newsTag.classList.add('active');  // 初期値のタグ要素（news_tag）に'active'クラスを追加
     }
 }
 
+// ▼newsTags配列の各要素に対しての処理▼
 newsTags.forEach(tag => {
     tag.addEventListener('click', () => {
-        // タグのアクティブクラスをリセット
+        // タグのactiveクラスをリセット
         newsTags.forEach(tag => tag.classList.remove('active'));
-        // クリックしたタグにアクティブクラスを追加
+        // クリックしたタグにactiveクラスを追加
         tag.classList.add('active');
 
-        const listClass = tag.classList[0].replace('_tag', '');
+
+        //クリックされたタグから、対応するニュースリストのnews_listを取得
+        let listClass = tag.classList[0].replace('_tag', '');  //info_tag内li要素のクラス名「_tag」を空文字に置き換えてnews_listに紐づける
         showList(listClass);
     });
 });
 
-// 初期化
-initializeActiveTag();
-showList('news');
+// 初期値として「news_tag」を表示
+initializeActiveTag();   // 最初のタグ「news_tag」にアクティブクラスを設定します
+showList('news');  // デフォルトでnewsを表示します
