248ad68d9ffb56ddf90ecf6a8d11695dc8c614dc
SB4inch <sakuragi.clip@gmail.com>
2024-06-03 16:20:36 +0900

    更新
    櫻木

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,1 +1,14 @@
 "use strict";
+$(function () {
+$(".slider").slick({
+    // autoplay: true,
+    arrows: false,
+    asNavFor: ".thumbnail",
+});
+    $(".thumbnail").slick({
+    arrows: false,
+    slidesToShow: 3,
+    asNavFor: ".slider",
+    focusOnSelect: true,
+});
+});
