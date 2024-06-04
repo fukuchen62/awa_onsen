f3ccb4e5a1d6cfa9a1d2764b894707475ec23fb3
sakiiiina3208 <matsuo.web2@gmail.com>
2024-06-04 20:20:26 +0900

    マイページ
    更新
    
    松尾

--- assets/js/tag_list.js
+++ assets/js/tag_list.js
@@ -1,38 +1,35 @@
 "use strict";
 
-let favoriteTags = document.querySelectorAll('.favorite_tag li');
-// .favorite_tag li 要素を全て取得
+// タグクリックイベントを設定
+const tags = document.querySelectorAll('.tag li');
+const contents = document.querySelectorAll('.contents');
 
-let favoriteLists = document.querySelectorAll('.favorite_list');
-// .favorite_list 要素を全て取得
+// 初期表示設定
+contents[0].classList.add('active');
+tags[0].classList.add('active');
+for (let i = 1; i < contents.length; i++) {
+    contents[i].classList.remove('active');
+}
 
-// デフォルトで onsen_tag が選択された状態
-favoriteTags[0].classList.add('active');
-// 最初の favoriteTags に active クラスを追加
-favoriteLists[0].classList.add('show');
-// 最初の favoriteLists に show クラスを追加
-
-favoriteTags.forEach(function (tag) {
-    // favoriteTags 各要素に対して実行
-    tag.addEventListener('click', function () {
-        // クリックイベントがあった場合に実行
-
-        // 全ての tag から active クラスを削除
-        favoriteTags.forEach(function (t) {
-            t.classList.remove('active');
+// タグクリックイベントハンドラ
+tags.forEach((tag, index) => {
+    tag.addEventListener('click', () => {
+        // 全てのコンテンツを非アクティブにする
+        contents.forEach(content => {
+            content.classList.remove('active');
         });
 
-        // 全ての favorite_list から show クラスを削除
-        favoriteLists.forEach(function (list) {
-            list.classList.remove('show');
-        });
+        // 選択中のクラスを解除
+        tags.forEach(tag => tag.classList.remove('active'));
 
-        // クリックされた tag に active クラスを追加
-        this.classList.add('active');
+        // クリックされたタグに対応するコンテンツをアクティブにする
+        contents[index].classList.add('active');
 
-        // クリックされた tag に対応する favorite_list に show クラスを追加
-        let listIndex = Array.from(favoriteTags).indexOf(this);
-        // クリックされた tag の index を取得
-        favoriteLists[listIndex].classList.add('show');
+        // クリックされたタグにクラスを付与
+        tag.classList.add('active');
     });
 });
+
+// お気に入り情報の取得と表示
+// ここで、お気に入り情報を取得するAPIを呼び出し、
+// データを元に各コンテンツを構築する処理を記述します。
