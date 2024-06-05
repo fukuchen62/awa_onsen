d0a360184da33c8a9a9ba928ea67ae07657f7c1c
sawaka527 <shuangshanxia7@gmail.com>
2024-06-05 13:52:24 +0900

    pc
    変更
    
    山下

--- assets/js/top.js
+++ assets/js/top.js
@@ -11,16 +11,62 @@ $(function () {
     });
 
     // nav
-    $(".hamburger").click(function(){
+    $(".hamburger").click(function () {
         $(this).toggleClass("active");
         $(".sp_nav .navlist").toggleClass("isactive");
         $(this).toggleClass("isactive");
     });
     // メニューの中をクリックしたら、メニューを閉じる
-    $(".navlist_item").click(function(){
+    $(".navlist_item").click(function () {
         $(".hamburger").removeClass("active");
         $(".sp_nav .navlist").removeClass("isactive");
         $(".hamburger").removeClass("isactive");
     });
+
+    // スクロールトップの設定
+    $('.top_button').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 'smooth');
+        return false;
+    });
+
+
+
+    $(window).scroll(function() {
+        // 画面が1201pxを超えているかどうかを確認
+        if ($(window).width() > 1200) {
+            let nav = $("#pc_nav");
+            let aboutSection = $(".about");
+            let threshold = aboutSection.offset().top - 50; // ナビゲーションを表示するセクションの位置
+
+            // スクロール位置が閾値を超えたら、ナビゲーションをスライドさせて表示
+            if ($(window).scrollTop() >= threshold) {
+                nav.css("right", "0");
+            } else {
+                nav.css("right", "-120%"); // スライドメニューを再び画面外に移動
+            }
+        }
+    });
+
+
+    // $(window).scroll(function() {
+    //     // 画面が1201pxを超えているかどうかを確認
+    //     if ($(window).width() > 1200) {
+    //         var nav = $(".navlist");
+    //         var aboutSection = $(".about");
+    //         var pcInner = $(".pc_inner");
+    //         var threshold = aboutSection.offset().top - 50; // ナビゲーションを表示するセクションの位置
+
+    //         // スクロール位置が閾値を超えたら、ナビゲーションを表示する
+    //         if ($(window).scrollTop() >= threshold) {
+    //             nav.appendTo(pcInner); // pc_innerの中に移動
+    //             nav.css({
+    //                 "right": "0"
+    //             });
+    //         } else {
+    //             nav.appendTo("body"); // bodyの直下に移動
+    //             nav.css("right", "-120%"); // スライドメニューを再び画面外に移動
+    //         }
+    //     }
+    // });
 
 });
