425d73df94a38a6ec656106422e476199f6fdce5
sasatoko <emthisa31.26@gmail.com>
2024-06-06 10:30:50 +0900

    シングルコラム
    シングルコラムとコメント
    
    江本

--- single-column.php
+++ single-column.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>コラム詳細</title>
-
-    <!-- google font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
-
-    <!-- リセットCSS -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css">
-    <!-- 共通CSS -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/common.css">
-    <!-- 個別CSS -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/column.css">
-</head>
-
 <body>
     <div class="bubble_background">
         <div class="container">
@@ -29,7 +8,8 @@
                 </a>
             </header>
 
-
+            <!-- header.phpを読み込む -->
+            <?php get_header(); ?>
             <!------------------------>
             <!-- ここからメインだよ -->
             <!------------------------>
@@ -264,13 +244,5 @@
 
 
 
-
-
-
-        <footer></footer>
-    </div>
-    <!-- javascriptの読み込み -->
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.js"></script>
-</body>
-
-</html>
+        <!-- footer.phpを読み込む -->
+        <?php get_footer(); ?>
