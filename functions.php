46dbd3cba3de31338099e34bdc1e8dcec2065fc3
tsurui16 <websystemkunren.mt16@gmail.com>
2024-06-06 15:13:55 +0900

    6月６日15：13まで
    テストデータ作成
    釣井

--- functions.php
+++ functions.php
@@ -51,6 +51,7 @@ add_filter(
  *
  * @return void
  */
+
 function fs_script_files()
 {
     // font-awesomeを読み込む
@@ -65,186 +66,108 @@ function fs_script_files()
         "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
     );
 
-    // reset.cssを読み込む
-    // wp_enqueue_style(
-    //     "foode-science-reset",
-    //     get_template_directory_uri() . "/assets/css/reset.css"
-    // );
-
-    // cssを読み込む
-    wp_enqueue_style(
-        "awa-onsen-about",
-        get_template_directory_uri() . "/assets/css/about.css"
-    );
-
-    wp_enqueue_style(
-        "awa-onsen-column_list",
-        get_template_directory_uri() . "/assets/css/column_list.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-column",
-        get_template_directory_uri() . "/assets/css/column.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-common",
-        get_template_directory_uri() . "/assets/css/common.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-course",
-        get_template_directory_uri() . "/assets/css/course.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-destyle",
-        get_template_directory_uri() . "/assets/css/destyle.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-form",
-        get_template_directory_uri() . "/assets/css/form.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-mypage",
-        get_template_directory_uri() . "/assets/css/mypage.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-nearby_list",
-        get_template_directory_uri() . "/assets/css/nearby_list.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-nearby",
-        get_template_directory_uri() . "/assets/css/nearby.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-news_list",
-        get_template_directory_uri() . "/assets/css/news_list.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-news",
-        get_template_directory_uri() . "/assets/css/news.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-privacypolicy",
-        get_template_directory_uri() . "/assets/css/privacypolicy.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-reset",
-        get_template_directory_uri() . "/assets/css/reset.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-result",
-        get_template_directory_uri() . "/assets/css/result.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-sauna",
-        get_template_directory_uri() . "/assets/css/sauna.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-search",
-        get_template_directory_uri() . "/assets/css/search.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-spa",
-        get_template_directory_uri() . "/assets/css/spa.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-thanks",
-        get_template_directory_uri() . "/assets/css/thanks.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-top",
-        get_template_directory_uri() . "/assets/css/top.css"
-    );
-    wp_enqueue_style(
-        "awa-onsen-spa_list",
-        get_template_directory_uri() . "/assets/css/spa_list.css"
-    );
-
+    // CSSファイルの読み込み
+    wp_enqueue_style("awa-onsen-reset", get_template_directory_uri() . "/assets/css/reset.css");
+    wp_enqueue_style("awa-onsen-destyle", get_template_directory_uri() . "/assets/css/destyle.css");
+    wp_enqueue_style("awa-onsen-common", get_template_directory_uri() . "/assets/css/common.css");
 
     // jQueryライブラリを読み込む
     wp_enqueue_script("jquery");
 
-    //WordPress 本体の jQuery を登録解除
-    // wp_deregister_script('jquery');
-
-    //jQuery を CDN から読み込む
-    // wp_enqueue_script(
-    //     'jquery',
-    //     '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-    //     array(),
-    //     '3.3.1',
-    //     true
-    // );
-
-    // jsを読み込む
-    wp_enqueue_script(
-        "awa-onsen-accordion",
-        get_template_directory_uri() . "/assets/js/accordion.js"
-    );
-    wp_enqueue_script(
-        "food-science-common",
-        get_template_directory_uri() . "/assets/js/common.js"
-    );
-    wp_enqueue_script(
-        "food-science-main",
-        get_template_directory_uri() . "/assets/js/main.js"
-    );
-    wp_enqueue_script(
-        "food-science-mypage",
-        get_template_directory_uri() . "/assets/js/mypage.js"
-    );
-    wp_enqueue_script(
-        "food-science-search",
-        get_template_directory_uri() . "/assets/js/search.js"
-    );
-    wp_enqueue_script(
-        "food-science-spa",
-        get_template_directory_uri() . "/assets/js/spa.js"
-    );
-    wp_enqueue_script(
-        "food-science-tag",
-        get_template_directory_uri() . "/assets/js/tag.js"
-    );
-    wp_enqueue_script(
-        "food-science-top",
-        get_template_directory_uri() . "/assets/js/top.js"
-    );
+    // JSの読み込み
+    wp_enqueue_script("awa-onsen-common", get_template_directory_uri() . "/assets/js/common.js");
+    wp_enqueue_script("awa-onsen-menu", get_template_directory_uri() . "/assets/js/menu.js");
 
 
-
-    // 画面毎に独自のスタイルシートとScriptファイルを読み込む
-    if (is_home()) {
-        // トップページの場合は
-        // wp_enqueue_style(
-        //     "foode-science-top",
-        //     get_template_directory_uri() . "/assets/css/top.css"
-        // );
-
-        // wp_enqueue_script(
-        //     'foode-science-top',
-        //     get_template_directory_uri() . "/assets/js/top.js",
-        //     '',
-        //     '',
-        //     true     //true＝フッターで出力
-        // );
+    // フロントページ用のCSSとJSを読み込む
+    if (is_front_page()) {
+        wp_enqueue_style('top-style', get_template_directory_uri() . '/assets/css/top.css');
+        wp_enqueue_script('top-script', get_template_directory_uri() . '/assets/js/top.js');
     }
 
-    if (is_page('concept')) {
-        // トップページの場合は
-        // wp_enqueue_style(
-        //     "foode-science-concept",
-        //     get_template_directory_uri() . "/assets/css/concept.css"
-        // );
+    // 詳細ページ用のCSSとJSの読み込み
+    if (is_singular('spa')) { // 温泉詳細のCSS,JSの読み込み
+        wp_enqueue_style('awa-onsen-spa', get_template_directory_uri() . '/assets/css/spa.css');
+        wp_enqueue_script('food-science-spa', get_template_directory_uri() . '/assets/js/spa.js');
+    }
 
-        // wp_enqueue_script(
-        //     'foode-science-concept',
-        //     get_template_directory_uri() . "/assets/js/concept.js",
-        //     '',
-        //     '',
-        //     true     //true＝フッターで出力
-        // );
+    if (is_singular('facility')) { // 周辺施設詳細のCSSの読み込み
+        wp_enqueue_style('awa-onsen-nearby', get_template_directory_uri() . '/assets/css/nearby.css');
+    }
+
+    if (is_singular('column')) { // コラムの詳細ページのCSSの読み込み
+        wp_enqueue_style('awa-onsen-column-one', get_template_directory_uri() . '/assets/css/column.css');
+    }
+
+    if (is_singular('course')) { // モデルコース詳細のCSS,JSの読み込み
+        wp_enqueue_style('awa-onsen-course', get_template_directory_uri() . '/assets/css/course.css');
+    }
+
+    // 一覧ページ用のCSSの読み込み
+    if (is_post_type_archive('spa')) { // 温泉一覧のCSSの読み込み
+        wp_enqueue_style('awa-onsen-spa_list', get_template_directory_uri() . '/assets/css/spa_list.css');
+    }
+
+    if (is_tax('facility_type')) { // 周辺施設一覧のCSSの読み込み
+        wp_enqueue_style('awa-onsen-nearby_list', get_template_directory_uri() . '/assets/css/nearby_list.css');
+    }
+
+    if (is_tax('column_type')) { // コラムの一覧ページのCSSとJSの読み込み
+        wp_enqueue_style('awa-onsen-column-list', get_template_directory_uri() . '/assets/css/column_list.css');
+        wp_enqueue_script('food-science-top', get_template_directory_uri() . '/assets/js/vendor/jquery-3.6.0.min.js');
+    }
+
+    if (is_tax('course_type')) { // モデルコース一覧のCSSの読み込み
+        wp_enqueue_style('awa-onsen-course_list', get_template_directory_uri() . '/assets/css/course_list.css');
+    }
+
+    if (is_category()) { // 新着一覧のCSSの読み込み
+        wp_enqueue_style('news-list-style', get_template_directory_uri() . '/assets/css/news_list.css');
+    }
+
+    // 検索ページ用のCSSとJSの読み込み
+    if (is_search()) {
+        wp_enqueue_style('search-style', get_template_directory_uri() . '/assets/css/search.css');
+        wp_enqueue_script('tag-list-script', get_template_directory_uri() . '/assets/js/tag_list.js');
+    }
+
+    // 固定ページ用のCSSとJSの読み込み
+    if (is_page('mypage')) { // マイページのCSS,JSの読み込み
+        wp_enqueue_style('awa-onsen-mypage', get_template_directory_uri() . '/assets/css/mypage.css');
+        wp_enqueue_script('food-science-mypage', get_template_directory_uri() . '/assets/js/mypage.js');
+    }
+
+    if (is_page('contact')) { // お問い合わせページのCSSの読み込み
+        wp_enqueue_style('awa-onsen-form', get_template_directory_uri() . '/assets/css/form.css');
+    }
+
+    if (is_page('confirm')) { // お問合せ確認のCSSの読み込み
+        wp_enqueue_style('awa-onsen-result', get_template_directory_uri() . '/assets/css/result.css');
+    }
+
+    if (is_page('about')) { // 自己紹介ページのCSSの読み込み
+        wp_enqueue_style('awa-onsen-about', get_template_directory_uri() . '/assets/css/about.css');
+    }
+
+    if (is_page('sauna')) { // サ活のページのCSSの読み込み
+        wp_enqueue_style('awa-onsen-sauna', get_template_directory_uri() . '/assets/css/sauna.css');
+    }
+
+    if (is_page('privacypolicy')) { // プライバシーポリシーのCSSの読み込み
+        wp_enqueue_style('awa-onsen-privacypolicy', get_template_directory_uri() . '/assets/css/privacypolicy.css');
+    }
+
+    // 新着情報のCSSの読み込み
+    if (is_single()) {
+        wp_enqueue_style('news-style', get_template_directory_uri() . '/assets/css/news.css');
     }
 }
-add_action('wp_enqueue_scripts', 'fs_script_files');
 
+add_action('wp_enqueue_scripts', 'fs_script_files');
+?>
+
+
+<?php
 /**
  * Contact form 7のときには、整形機能をOffにする
  *
@@ -255,8 +178,8 @@ function fs_wpcf7_autop()
     return false;
 }
 add_filter(
-    'wpcf7_autop_or_not',     //関数の呼び出すタイミング
-    'fs_wpcf7_autop'  //呼び出す関数名
+    'wpcf7_autop_or_not', // 関数の呼び出すタイミング
+    'fs_wpcf7_autop' // 呼び出す関数名
 );
 
 function the_company_name()
@@ -268,3 +191,4 @@ function get_company_name()
 {
     return "株式会社ＱＬＩＰインタナショナル";
 }
+?>
