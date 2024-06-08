2a44dc8e54a5a59e2aff46788aaf895314634689
tsurui16 <websystemkunren.mt16@gmail.com>
2024-06-08 17:24:58 +0900

    6月8日17：24
    テストデータ作成
    釣井

--- taxonomy-course_type.php
+++ taxonomy-course_type.php
@@ -9,16 +9,30 @@
         <?php get_template_part('template-parts/breadcrumb') ?>
         <!-- タグ -->
         <ul class="info_tag">
-            <li class="touring_tag">ツーリング</li>
-            <li class="rafting_tag">ラフティング</li>
-            <li class="lookpretty_tag">映える</li>
-            <li class="hiking_tag">ハイキング</li>
-        </ul>
+                    <li class="touring_tag"><a href="http://localhost/awa_onsen/course_type/tour/">ツーリング</a></li>
+                    <li class="rafting_tag"><a href="#">ラフティング</a></li>
+                    <li class="lookpretty_tag"><a href="#">映える</a></li>
+                    <li class="hiking_tag"><a href="#">ハイキング</a></li>
+                </ul>
 
         <!-- 一覧 -->
         <div class="modelcourse_list">
             <article class="card">
+                <!-- WordPressのルールの開始 -->
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : ?>
+                    <?php the_post(); ?>
 
+                    <!-- カード型の開始 -->
+                    <?php get_template_part('template-parts/loop', 'course'); ?>
+                    <!-- カード型の終了 -->
+
+                    <!-- WordPressのルールの終了 -->
+                    <?php endwhile; ?>
+                    <?php endif; ?>
+            </article>
+
+            <!-- <article class="card">
                 <a href="https://translate.google.co.jp/?hl=ja&tab=TT&sl=auto&tl=en&op=translate">
                     <div>
                         <span></span>
@@ -26,34 +40,8 @@
                     </div>
                     <h3>最南端！！空と海を感じるツーリングコース！！！</h3>
                 </a>
-            </article>
-            <article class="card">
-                <a href="https://translate.google.co.jp/?hl=ja&tab=TT&sl=auto&tl=en&op=translate">
-                    <div>
-                        <span></span>
-                        <img src="../assets/images/onsen_img.jpg" alt="" />
-                    </div>
-                    <h3>最南端！！空と海を感じるツーリングコース！！！</h3>
-                </a>
-            </article>
-            <article class="card">
-                <a href="https://translate.google.co.jp/?hl=ja&tab=TT&sl=auto&tl=en&op=translate">
-                    <div>
-                        <span></span>
-                        <img src="../assets/images/onsen_img.jpg" alt="" />
-                    </div>
-                    <h3>最南端！！空と海を感じるツーリングコース！！！</h3>
-                </a>
-            </article>
-            <article class="card">
-                <a href="https://translate.google.co.jp/?hl=ja&tab=TT&sl=auto&tl=en&op=translate">
-                    <div>
-                        <span></span>
-                        <img src="../assets/images/onsen_img.jpg" alt="" />
-                    </div>
-                    <h3>最南端！！空と海を感じるツーリングコース！！！</h3>
-                </a>
-            </article>
+            </article> -->
+<!-- 
             <article class="card">
                 <a href="https://translate.google.co.jp/?hl=ja&tab=TT&sl=auto&tl=en&op=translate">
                     <div>
@@ -64,16 +52,37 @@
                 </a>
             </article>
 
+            <article class="card">
+                <a href="https://translate.google.co.jp/?hl=ja&tab=TT&sl=auto&tl=en&op=translate">
+                    <div>
+                        <span></span>
+                        <img src="../assets/images/onsen_img.jpg" alt="" />
+                    </div>
+                    <h3>最南端！！空と海を感じるツーリングコース！！！</h3>
+                </a>
+            </article>
 
+            <article class="card">
+                <a href="https://translate.google.co.jp/?hl=ja&tab=TT&sl=auto&tl=en&op=translate">
+                    <div>
+                        <span></span>
+                        <img src="../assets/images/onsen_img.jpg" alt="" />
+                    </div>
+                    <h3>最南端！！空と海を感じるツーリングコース！！！</h3>
+                </a>
+            </article>
+
+ -->
 
         </div>
 
     </section>
 
-    <!-- ページネーション -->
-    <p class="pagination">
-        << 1　2　3　4　5>>
-    </p>
+    <!-- ぺージナビゲーション -->
+    <?php if (function_exists('wp_pagenavi')) : ?>
+                    <div class="pagination">
+                        <?php wp_pagenavi(); ?>
+                    </div>
 
 
 </main>
