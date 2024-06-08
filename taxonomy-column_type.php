d13c4043b8a6d6e12f31b2f92c1509939335be00
sasatoko <emthisa31.26@gmail.com>
2024-06-08 17:19:37 +0900

    お問い合わせ確認ページとコラム一覧
    加工中
    江本

--- taxonomy-column_type.php
+++ taxonomy-column_type.php
@@ -1,136 +1,144 @@
 <!-- header.phpを読み込む -->
 <?php get_header(); ?>
 
-<!-- WordPressのルールの開始 -->
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : ?>
-        <?php the_post(); ?>
 
 
-        <main>
-            <section>
-                <h2 class="under_line">コラム一覧</h2>
+<main class="pc_inner">
+    <div class="container">
 
-                <!-- パンくずリスト -->
-                <?php get_template_part('template-parts/breadcrumb'); ?>
+        <section>
+            <h2 class="under_line">コラム一覧</h2>
+
+            <!-- パンくずリスト -->
+            <?php get_template_part('template-parts/breadcrumb'); ?>
+
+            <!-- WordPressのルールの開始 -->
+            <?php if (have_posts()) : ?>
 
                 <!-- タグ -->
-                <ul class="info_tag">
-                    <li class="news_tag"><a href="">温泉</a></li>
-                    <li class="gourmet_tag"><a href="">温泉周辺<br>施設紹介</a></li>
-                    <li class="play_tag"><a href="">お役立ち</a></li>
+                <ul class="tag element03">
+                    <li class="active"><a href="#">温泉</a></li>
+                    <li><a href="#">温泉周辺<br>施設紹介</a></li>
+                    <li><a href="#">お役立ち</a></li>
                 </ul>
 
                 <!-- 一覧 -->
                 <div class="news_list">
-                    <article class="news_card">
-                        <a href="https://ebookjapan.yahoo.co.jp/">
-                            <img src="../assets/images/onsen_img.jpg" alt="" />
-                            <div class="news_contents">
-                                <p class="date fugaz-one-regular"><?php the_time('Y-m-d-H:i'); ?></p>
-                                <p class="title"><?php the_title(); ?></p>
-                                <div class="hashtag_list .flex">
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag"><?php the_content(); ?>#温泉</a>
-                                    </object>
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#東部</a>
-                                    </object>
-                                </div>
+
+                    <!-- 記事があるとき -->
+                    <?php while (have_posts()) : ?>
+                        <?php the_post(); ?>
+
+                        <article <?php post_class('news_card'); ?>>
+                            <!-- コラム画像getここから -->
+                            <?php
+                            $pic = get_field('column_pic');
+                            $pic_url = $pic['sizes']['thumbnail'];
+                            ?>
+
+                            <!-- コラム画像getここまで-->
+                            <a href=" <?php the_permalink(); ?>"><img src="<?php echo $pic_url; ?>" alt=""></a>
+                            <div class="news_cintents">
+                                <a href="<?php the_permalink(); ?>">
+                                    <p class="date fugaz-one-regular"><?php echo get_post_time('Y.m.d.D H:i'); ?></p>
+                                    <h6 class="title"><?php the_title(); ?></h6>
+                                </a>
+                                <!-- ハッシュタグ取得記述をする -->
+
+                                <?php
+                                $categories = get_the_category();
+                                if ($categories) : ?>
+
+                                    <div class="hashtag_list">
+
+                                        <?php foreach ($categories as $category) : ?>
+
+                                            <a href="https://fontawesome.com/" class="hashtag"><?php echo $category->name; ?>#fontawesome</a>
+                                            <a href="https://cdnjs.com/" class="hashtag">#cdn</a>
+                                            <a href="https://tech-unlimited.com/color.html" class="hashtag">#ジェネレーター</a>
+                                    </div>
+                                <?php endforeach; ?>
                             </div>
-                        </a>
+                        <?php endif; ?>
+                        </article>
+                    <?php endwhile; ?>
+                    <!-- 一つの記事ここまで -->
+
+
+
+                    <article class="news_card">
+                        <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92"><img src="../assets/images/bike.jpg" alt=""></a>
+                        <div class="news_cintents">
+                            <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92">
+                                <p class="date fugaz-one-regular">2024.06.05.mon 10:00</p>
+                                <h6 class="title">講習会管理システムのログイン画面にいきます！</h6>
+                            </a>
+                            <div class="hashtag_list">
+                                <a href="https://fontawesome.com/" class="hashtag">#fontawesome</a>
+                                <a href="https://cdnjs.com/" class="hashtag">#cdn</a>
+                                <a href="https://tech-unlimited.com/color.html" class="hashtag">#ジェネレーター</a>
+                            </div>
+                        </div>
                     </article>
                     <article class="news_card">
-                        <a href="https://ebookjapan.yahoo.co.jp/">
-                            <img src="../assets/images/onsen_img.jpg" alt="" />
-                            <div class="news_contents">
-                                <p class="date fugaz-one-regular">2024.05.24 10:00</p>
-                                <p class="title">尾花温泉夏季特別営業のお知らせ</p>
-                                <div class="hashtag_list .flex">
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#温泉</a>
-                                    </object>
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#西部</a>
-                                    </object>
-                                </div>
+                        <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92"><img src="../assets/images/bike.jpg" alt=""></a>
+                        <div class="news_cintents">
+                            <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92">
+                                <p class="date fugaz-one-regular">2024.06.05.mon 10:00</p>
+                                <h6 class="title">講習会管理システムのログイン画面にいきます！</h6>
+                            </a>
+                            <div class="hashtag_list">
+                                <a href="https://fontawesome.com/" class="hashtag">#fontawesome</a>
+                                <a href="https://cdnjs.com/" class="hashtag">#cdn</a>
+                                <a href="https://tech-unlimited.com/color.html" class="hashtag">#ジェネレーター</a>
                             </div>
-                        </a>
+                        </div>
                     </article>
                     <article class="news_card">
-                        <a href="https://ebookjapan.yahoo.co.jp/">
-                            <img src="../assets/images/onsen_img.jpg" alt="" />
-                            <div class="news_contents">
-                                <p class="date fugaz-one-regular">2024.05.24 10:00</p>
-                                <p class="title">温泉情報に「安岡温泉」が追加されました。</p>
-                                <div class="hashtag_list .flex">
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#温泉</a>
-                                    </object>
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#温泉</a>
-                                    </object>
-                                </div>
+                        <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92"><img src="../assets/images/bike.jpg" alt=""></a>
+                        <div class="news_cintents">
+                            <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92">
+                                <p class="date fugaz-one-regular">2024.06.05.mon 10:00</p>
+                                <h6 class="title">講習会管理システムのログイン画面にいきます！</h6>
+                            </a>
+                            <div class="hashtag_list">
+                                <a href="https://fontawesome.com/" class="hashtag">#fontawesome</a>
+                                <a href="https://cdnjs.com/" class="hashtag">#cdn</a>
+                                <a href="https://tech-unlimited.com/color.html" class="hashtag">#ジェネレーター</a>
                             </div>
-                        </a>
+                        </div>
                     </article>
                     <article class="news_card">
-                        <a href="https://ebookjapan.yahoo.co.jp/">
-                            <img src="../assets/images/onsen_img.jpg" alt="" />
-                            <div class="news_contents">
-                                <p class="date fugaz-one-regular">2024.05.24 10:00</p>
-                                <p class="title">温泉情報に「安岡温泉」が追加されました。</p>
-                                <div class="hashtag_list .flex">
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#温泉</a>
-                                    </object>
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#温泉</a>
-                                    </object>
-                                </div>
+                        <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92"><img src="../assets/images/bike.jpg" alt=""></a>
+                        <div class="news_cintents">
+                            <a href="http://bzclass.bizan.com/adm/mainte.asp?comp_id=1&koza_id=92">
+                                <p class="date fugaz-one-regular">2024.06.05.mon 10:00</p>
+                                <h6 class="title">講習会管理システムのログイン画面にいきます！</h6>
+                            </a>
+                            <div class="hashtag_list">
+                                <a href="https://fontawesome.com/" class="hashtag">#fontawesome</a>
+                                <a href="https://cdnjs.com/" class="hashtag">#cdn</a>
+                                <a href="https://tech-unlimited.com/color.html" class="hashtag">#ジェネレーター</a>
                             </div>
-                        </a>
+                        </div>
                     </article>
-                    <article class="news_card">
-                        <a href="https://ebookjapan.yahoo.co.jp/">
-                            <img src="../assets/images/onsen_img.jpg" alt="" />
-                            <div class="news_contents">
-                                <p class="date fugaz-one-regular">2024.05.24 10:00</p>
-                                <p class="title">温泉情報に「安岡温泉」が追加されました。</p>
-                                <div class="hashtag_list .flex">
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#温泉</a>
-                                    </object>
-                                    <object>
-                                        <a href="https://www.yahoo.co.jp/" class="hashtag">#温泉</a>
-                                    </object>
-                                </div>
-                            </div>
-                        </a>
-                    </article>
+
                 </div>
+        </section>
+    <?php endif; ?>
 
-            </section>
 
-            <!-- ページネーション -->
-            <p class="pagination">
-                << 1　2　3　4　5>>
-            </p>
+    <!-- ページネーション -->
+    <?php if (function_exists('wp_pagenavi')) : ?>
+        <p class="pagination"><?php wp_pagenavi(); ?> &lt;&lt; 1　2　3　4　5 &gt;&gt; </p>
+    <?php endif; ?>
+    <button class="back_btn" onclick="history.back">
+        <span><i class="fa-solid fa-arrow-left"></i>back</span>
+    </button>
+    </div>
+</main>
 
-            <button class="back_btn" onclick="history.back">
-                <p><i class="fa-solid fa-arrow-left"></i>back</p>
-            </button>
-        </main>
-        <!-- WordPressのルールの終了 -->
-    <?php endwhile; ?>
-<?php endif; ?>
 </div>
-<footer></footer>
-</div>
-<script src="../assets/js/vendor/jquery-3.6.0.min.js"></script>
 
-<script src="../assets/js/common.js"></script>
-
-</body>
-
-</html>
+<!-- footer.phpを読み込む -->
+<?php get_footer(); ?>
