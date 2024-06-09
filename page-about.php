66d9a060e9b0e929f2051281fcb29a6c2d960b94
tsurui16 <websystemkunren.mt16@gmail.com>
2024-06-09 18:56:22 +0900

    6月9日18:56
    テストデータ作成
    釣井

--- page-about.php
+++ page-about.php
@@ -1,52 +1,30 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php
+/*
+Template Name: About Page Template
+*/
+get_header();
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>このサイトについて</title>
+<!-- main -->
+<main class="pc_inner">
+    <div class="container">
+        <!-- タイトルを表示 -->
+        <h2 class="under_line"><?php the_title(); ?></h2>
 
-    <!-- google font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+        <!-- パンくず -->
+        <?php get_template_part('template-parts/breadcrumb') ?>
 
-    <!-- リセットCSS -->
-    <link rel="stylesheet" href="../assets/css/destyle.css">
-    <!-- 共通css -->
-    <link rel="stylesheet" href="../assets/css/common.css">
-    <!-- 個別CSS -->
-    <link rel="stylesheet" href="../assets/css/about.css">
-</head>
+        <!-- 最初の本文を表示 -->
+        <p><?php the_content(); ?></p>
 
-<body>
-    <div class="bubble_background">
-        <div class="container">
+        <!-- カスタムフィールドから見出しを取得し表示 -->
+        <h2 class="under_line"><?php echo get_post_meta(get_the_ID(), 'subheading', true); ?></h2>
 
-            <!-- header -->
-            <header>
-                <a href="#">
-                    <img class="logo" src="../assets/images/logo.svg" alt="サイトロゴ">
-                </a>
-            </header>
+        <!-- カスタムフィールドから2つ目の本文を取得し表示 -->
+        <p><?php echo wpautop(get_post_meta(get_the_ID(), 'content_2', true)); ?></p>
 
-            <main>
-                <h2 class="under_line">このサイトについて</h2>
-
-                <!-- パンくず -->
-
-                <p>このサイトは国の求職者支援制度に基づく、株式会社QLIP クリッププログラミングスクール・WEBシステム開発実践科Ⅱ第8期生、『集（つどい）』班が、2024年に制作したウェブサイトです。</p>
-
-                <h2 class="under_line">サイト制作の経緯</h2>
-
-                <p>徳島県には多くの温泉がありますが、多くの県民または県外客には存在が知られていなかったり、温泉だけのために遠くまで足を運ぶ事が面倒といったイメージを持つ方が多いように感じます。県民だけでなく近隣県の方に徳島の温泉の魅力とその周辺情報を発信して、少しでも徳島に興味を抱いて、来県していただくことにより地域の活性化に貢献したいとの思いで「あわあわ温泉ぶらり」のサイトを企画しました。</p>
-            </main>
-
-        </div>
-        <footer></footer>
     </div>
-    <!-- javascriptの読み込み -->
-    <script src="../assets/js/common.js"></script>
-</body>
+</main>
 
-</html>
+<!-- footer.phpを読み込む -->
+<?php get_footer(); ?>
