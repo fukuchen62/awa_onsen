184cc089d68189a98ab518b131d7ce7deb6126e1
tsurui16 <websystemkunren.mt16@gmail.com>
2024-06-10 13:50:06 +0900

    6月10日13：49
    テストデータ作成
    釣井

--- page-about.php
+++ page-about.php
@@ -17,11 +17,6 @@ get_header();
         <!-- 最初の本文を表示 -->
         <p><?php the_content(); ?></p>
 
-        <!-- カスタムフィールドから見出しを取得し表示 -->
-        <h2 class="under_line"><?php echo get_post_meta(get_the_ID(), 'subheading', true); ?></h2>
-
-        <!-- カスタムフィールドから2つ目の本文を取得し表示 -->
-        <p><?php echo wpautop(get_post_meta(get_the_ID(), 'content_2', true)); ?></p>
 
     </div>
 </main>
