32ce9fdc3079bf2955ea9a06af72726f50a8bd2f
tsurui16 <websystemkunren.mt16@gmail.com>
2024-06-12 11:42:36 +0900

    6月12日11：42
    モデルコース詳細
    釣井

--- single-course.php
+++ single-course.php
@@ -184,17 +184,15 @@
                         </div>
                         <div class="flex_car">
                             <div class="square_green"></div>
-                            <p>公式HP：</p>
+                            <p>公式HP：<?php
+                                    $official_website = get_field('course_url' . $i);
+                                    if (!empty($official_website)) {
+                                        echo '<a href="' . esc_url($official_website) . '" target="_blank" rel="noopener noreferrer">' . esc_html($official_website) . '</a>';
+                                    } else {
+                                        echo '公式HPはありません。';
+                                    }
+                                    ?></p>
                         </div>
-                        <p><?php
-                            $official_website = get_field('course_url' . $i);
-                            if (!empty($official_website)) {
-                                echo '<a href="' . esc_url($official_website) . '" target="_blank" rel="noopener noreferrer">' . esc_html($official_website) . '</a>';
-                            } else {
-                                echo '公式HPはありません。';
-                            }
-                            ?>
-                        </p>
 
                         <div class="flex greencar">
                             <div class="car_green"></div>
@@ -337,17 +335,15 @@
                         </div>
                         <div class="flex_car">
                             <div class="square_green"></div>
-                            <p>公式HP：</p>
+                            <p>公式HP：<?php
+                                    $official_website = get_field('course_url' . $i);
+                                    if (!empty($official_website)) {
+                                        echo '<a href="' . esc_url($official_website) . '" target="_blank" rel="noopener noreferrer">' . esc_html($official_website) . '</a>';
+                                    } else {
+                                        echo '公式HPはありません。';
+                                    }
+                                    ?></p>
                         </div>
-                        <p><?php
-                            $official_website = get_field('course_url' . $i);
-                            if (!empty($official_website)) {
-                                echo '<a href="' . esc_url($official_website) . '" target="_blank" rel="noopener noreferrer">' . esc_html($official_website) . '</a>';
-                            } else {
-                                echo '公式HPはありません。';
-                            }
-                            ?>
-                        </p>
 
                         <div class="flex greencar">
                             <div class="car_green"></div>
@@ -362,8 +358,162 @@
 
         <!-- 関連店舗 -->
         <h5>今回の関連店舗はこちら</h5>
-        <!-- カード -->
+        <!-- カード型 -->
         <div class="article_all">
+            <?php
+            // ループの回数を定義
+            $loop_count = 4;
+
+            // すべてのカスタム投稿タイプを取得
+            $custom_post_types = get_post_types(array('_builtin' => false));
+
+            for ($i = 1; $i <= $loop_count; $i++) {
+                // カスタムフィールドの名前を生成
+                $field_name = 'url' . $i;
+                $slug = get_field($field_name); // ここにカスタムフィールドの値が入る
+
+                if ($slug) {
+                    // カスタムクエリで投稿を検索
+                    $args = array(
+                        'name' => $slug,
+                        'post_type' => $custom_post_types,
+                        'post_status' => 'publish',
+                        'numberposts' => 1
+                    );
+
+                    $posts = get_posts($args);
+
+                    if (!empty($posts)) {
+                        $post = $posts[0]; // 最初の投稿を取得
+                        setup_postdata($post);
+
+                        // 投稿情報を取得
+                        $post_id = $post->ID;
+                        $post_title = get_the_title($post_id);
+                        $post_link = get_permalink($post_id);
+                        $post_thumbnail = get_the_post_thumbnail($post_id, 'full'); // フルサイズのアイキャッチ画像を取得
+                        $post_type = get_post_type($post_id); // カスタム投稿タイプ名を取得
+            ?>
+                        <article class="card <?php echo esc_attr($post_type); ?>">
+                            <a href="<?php echo esc_url($post_link); ?>">
+                                <div>
+                                    <span></span>
+                                    <?php if ($post_thumbnail) : ?>
+                                        <img src="<?php echo esc_url(get_the_post_thumbnail_url($post_id, 'full')); ?>" alt="<?php echo esc_attr($post_title); ?>" />
+                                    <?php endif; ?>
+                                </div>
+                                <h3><?php echo esc_html($post_title); ?></h3>
+                            </a>
+                        </article>
+            <?php
+                        wp_reset_postdata();
+                    }
+                }
+            }
+            ?>
+
+        </div>
+        </section>
+
+        <!-- 関連するコラム、お知らせ -->
+        <section class="connection_column">
+            <h5>関連コラム、情報
+            </h5>
+            <?php
+            // カスタム投稿タイプ 'column' から投稿を取得
+            // $args = array(
+            //     'post_type' => 'column', // カスタム投稿タイプ 'column'
+            //     'posts_per_page' => -1, // すべての投稿を取得
+            // );
+            // $column_query = new WP_Query($args);
+
+            // if ($column_query->have_posts()) :
+            //     while ($column_query->have_posts()) : $column_query->the_post();
+            //         // アイキャッチ画像のURLを取得
+            //         $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'); // サムネイルサイズを小さく設定
+            //         // 投稿の日付を取得
+            //         $post_date = get_the_date('Y.m.d D H:i');
+            //         // 投稿のタイトルを取得
+            //         $post_title = get_the_title();
+            //         // 投稿のパーマリンクを取得
+            //         $post_permalink = get_permalink();
+            //         // 投稿のタクソノミースラッグを取得
+            //         $tags = get_the_terms(get_the_ID(), 'column_type');
+
+            //
+            //新しいロジック
+            // ループの回数を定義
+            // カスタムフィールドを4つ作ってるので4回設定
+            $loop_count = 4;
+
+            // すべてのカスタム投稿タイプを取得
+            $custom_post_types = get_post_types(array('_builtin' => false));
+            for ($i = 1; $i <= $loop_count; $i++) {
+                // カスタムフィールドで設定したフィールド名＋カウントの数字
+                // 重複しないようにcolumn∔1~4の形で設定
+                $field_name = 'column' . $i;
+                $slug = get_field($field_name); // ここにカスタムフィールドの値が入る
+
+                if ($slug) {
+                    // カスタムクエリで投稿を検索
+                    $args = array(
+                        'name' => $slug,
+                        'post_type' => $custom_post_types,
+                        'post_status' => 'publish',
+                        'numberposts' => 1
+                    );
+                    $posts = get_posts($args);
+                    if (!empty($posts)) { //記事があるとき
+                        $post = $posts[0]; // 最初の投稿を取得
+                        setup_postdata($post);
+                        // 投稿情報を取得
+                        $post_id = $post->ID;
+                        // 投稿のタイトルを取得
+                        $post_title = get_the_title($post_id);
+                        // 投稿のパーマリンクを取得
+                        $post_link = get_permalink($post_id);
+                        // 投稿の日付を取得
+                        $post_date = get_the_date('Y.m.d D H:i');
+                        // アイキャッチ画像のURLを取得
+                        $post_thumbnail = get_the_post_thumbnail_url($post_id, 'thumbnail');
+                        // 投稿のタクソノミースラッグを取得
+                        $tags = get_the_terms(get_the_ID(), 'column_type');
+            ?>
+                        <!-- ページ下部の関係コラム枠 -->
+                        <article class="news_card news">
+                            <!-- コラムへのリンク -->
+                            <a href="<?php echo esc_url($post_link); ?>">
+                                <!-- アイキャッチ取得 -->
+                                <?php if ($post_thumbnail) : ?>
+                                    <img src="<?php echo esc_url($post_thumbnail); ?>" alt="<?php echo esc_attr($post_title); ?>">
+                                <?php endif; ?>
+                            </a>
+                            <div class="news_contents">
+                                <a href="<?php echo esc_url($$post_link); ?>">
+                                    <!-- 日付と時間 -->
+                                    <p class="date fugaz-one-regular"><?php echo esc_html($post_date); ?></p>
+                                    <!-- 記事タイトル -->
+                                    <h6 class="title"><?php echo esc_html($post_title); ?></h6>
+                                </a>
+                                <!-- ハッシュタグ -->
+                                <div class="hashtag_list">
+                                    <?php if ($tags && !is_wp_error($tags)) : ?>
+                                        <?php foreach ($tags as $tag) : ?>
+                                            <a href="<?php echo esc_url(get_term_link($tag)); ?>" class="hashtag">#<?php echo esc_html($tag->name); ?></a>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </article>
+            <?php
+                        wp_reset_postdata();
+                    } else {
+                        // 記事が見つからない場合の代替メッセージを表示
+                        echo '<p>コラム、情報はまだありません。</p>';
+                    }
+                }
+            }
+            ?>
 
             <!-- バックボタン -->
             <?php
@@ -395,7 +545,7 @@
             <?php
             }
             ?>
-        </div>
+    </div>
 </main>
 
 <!-- footer.phpを読み込む -->
