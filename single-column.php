acae87748b4c732dc1cc9462ed27776830f7bf64
keisukematsuka <matsukakeisuke0202@gmail.com>
2024-06-14 10:42:20 +0900

    シングルコラム
    画像の配列のエラーコード解消

--- single-column.php
+++ single-column.php
@@ -12,33 +12,30 @@
 
             <!-- パンくず -->
             <?php get_template_part('template-parts/breadcrumb'); ?>
+
             <!-- コラム写真 -->
             <div class="column_img">
-
-                <!-- コラム画像ここから -->
                 <?php
                 $pic = get_field('column_pic');
-                $pic_url = $pic['sizes']['large'];
-                $name = get_field('slug');
+                if ($pic) {
+                    $pic_url = $pic['sizes']['large'];
+                    $name = get_field('slug');
                 ?>
-                <img src="<?php echo $pic_url; ?>" alt="<?php $name; ?>">
-                <!-- コラム画像ここまで-->
-
+                    <img src="<?php echo esc_url($pic_url); ?>" alt="<?php echo esc_attr($name); ?>">
+                <?php
+                }
+                ?>
             </div>
 
-
-            <!-- タグ -->
+            <!-- ハッシュタグ -->
             <div class="hashtag_list">
                 <?php
                 // 現在の投稿のIDを取得
                 $post_id = get_the_ID();
-
                 // 投稿タイプを取得
                 $post_type = get_post_type($post_id);
-
                 // 投稿タイプに関連するタクソノミーを取得
                 $taxonomies = get_object_taxonomies($post_type);
-
                 if (!empty($taxonomies)) {
                     foreach ($taxonomies as $taxonomy) {
                         // タクソノミーに関連するタームを取得
@@ -49,7 +46,6 @@
                                 <span class="hashtag"><?php echo esc_html($term->name); ?></span>
                 <?php
                             }
-                            echo '</ul>';
                         }
                     }
                 }
@@ -57,40 +53,34 @@
             </div>
 
             <!-- 記事内容 -->
-            <p class="column_text"><?php the_field('column_description'); ?></p>
+            <?php if (get_field('column_description')) : ?>
+                <p class="column_text"><?php the_field('column_description'); ?></p>
+            <?php endif; ?>
 
         </section>
 
         <!-- コメントフォーム -->
         <div class="comment_form">
-            <!-- コメント入力欄 -->
-            <!-- form action= -->
             <div class="item">
                 <label class="comment" for="comment">
-                    <!-- コメント入力フォームとコメントリスト -->
-                    <?php comments_template(); ?></label>
-                <!-- <textarea rows="10" cols="80" name="comment" id="comment"></textarea><br> -->
+                    <?php comments_template(); ?>
+                </label>
             </div>
-
-
         </div>
 
         <!-- 関連情報 -->
-        <!-- <section>
+        <section>
             <h5>関連情報</h5>
             <div class="article_all">
                 <?php
                 // ループの回数を定義
                 $loop_count = 4;
-
                 // すべてのカスタム投稿タイプを取得
                 $custom_post_types = get_post_types(array('_builtin' => false));
-
                 for ($i = 1; $i <= $loop_count; $i++) {
                     // カスタムフィールドの名前を生成
                     $field_name = 'url' . $i;
                     $slug = get_field($field_name); // ここにカスタムフィールドの値が入る
-
                     if ($slug) {
                         // カスタムクエリで投稿を検索
                         $args = array(
@@ -99,13 +89,10 @@
                             'post_status' => 'publish',
                             'numberposts' => 1
                         );
-
                         $posts = get_posts($args);
-
                         if (!empty($posts)) {
                             $post = $posts[0]; // 最初の投稿を取得
                             setup_postdata($post);
-
                             // 投稿情報を取得
                             $post_id = $post->ID;
                             $post_title = get_the_title($post_id);
@@ -132,106 +119,30 @@
                     }
                 }
                 ?>
+            </div>
 
-            </div> -->
-
-
-        <!--  -->
-        <?php
-        // ACFからカスタムフィールドの値を取得
-        $string = get_field('column_url');
-
-        // 初期化
-        $external_url = '';
-        $link_text = '';
-
-        // explode関数を使用して文字列を分割し、分割した結果の要素数をチェック
-        if ($string) {
-            $parts = explode(",", $string);
-            if (count($parts) == 2) {
-                $external_url = $parts[0];
-                $link_text = $parts[1];
-            }
-        }
-        ?>
-
-        <?php if (!empty($external_url) && !empty($link_text)) : ?>
-
-            <!-- フロント班へ 以下関連ウェブサイトがあった場合表示される項目 -->
-
-            <h5 class="mt32">関連ウェブサイト</h5>
-            <p>関連ウェブサイト:
-                <a href="<?php echo esc_url($external_url); ?>" target="_blank"><?php echo esc_html($link_text); ?></a>
-            </p>
-
-            <!-- ここまで -->
-        <?php endif; ?>
-
-        </section>
-
-        <section class="recommend">
             <?php
-            // ループの回数を定義
-            $loop_count = 6;
-
-            // すべてのカスタム投稿タイプを取得
-            $custom_post_types = get_post_types(array('_builtin' => false));
-
-            // 投稿があるかどうかのフラグ
-            $has_posts = false;
-
-            for ($i = 1; $i <= $loop_count; $i++) {
-                // カスタムフィールドの名前を生成
-                $field_name = 'url' . $i;
-                $slug = get_field($field_name); // ここにカスタムフィールドの値が入る
-
-                if ($slug) {
-                    // カスタムクエリで投稿を検索
-                    $args = array(
-                        'name' => $slug,
-                        'post_type' => $custom_post_types,
-                        'post_status' => 'publish',
-                        'numberposts' => 1
-                    );
-
-                    $posts = get_posts($args);
-
-                    if (!empty($posts)) {
-                        if (!$has_posts) {
-                            $has_posts = true;
-                            echo '<h5>こちらもいかがでしょうか？</h5>';
-                            echo '<div class="article_all">';
-                        }
-
-                        $post = $posts[0]; // 最初の投稿を取得
-                        setup_postdata($post);
-
-                        // 投稿情報を取得
-                        $post_id = $post->ID;
-                        $post_title = get_the_title($post_id);
-                        $post_link = get_permalink($post_id);
-                        $post_thumbnail = get_the_post_thumbnail($post_id, 'full'); // フルサイズのアイキャッチ画像を取得
-                        $post_type = get_post_type($post_id); // カスタム投稿タイプ名を取得
-            ?>
-                        <article class="card spa <?php echo esc_attr($post_type); ?>">
-                            <a href="<?php echo esc_url($post_link); ?>">
-                                <div>
-                                    <span></span>
-                                    <?php if ($post_thumbnail) : ?>
-                                        <img src="<?php echo esc_url(get_the_post_thumbnail_url($post_id, 'full')); ?>" alt="<?php echo esc_attr($post_title); ?>" />
-                                    <?php else : ?>
-                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage.png" alt="<?php the_title(); ?>" />
-                                    <?php endif; ?>
-                                </div>
-                                <h3><?php echo ($post_title); ?></h3>
-                            </a>
-                        </article>
-            <?php
-                        wp_reset_postdata();
-                    }
+            // ACFからカスタムフィールドの値を取得
+            $string = get_field('column_url');
+            // 初期化
+            $external_url = '';
+            $link_text = '';
+            // explode関数を使用して文字列を分割し、分割した結果の要素数をチェック
+            if ($string) {
+                $parts = explode(",", $string);
+                if (count($parts) == 2) {
+                    $external_url = $parts[0];
+                    $link_text = $parts[1];
                 }
             }
             ?>
+
+            <?php if (!empty($external_url) && !empty($link_text)) : ?>
+                <h5 class="mt32">関連ウェブサイト</h5>
+                <p>関連ウェブサイト:
+                    <a href="<?php echo esc_url($external_url); ?>" target="_blank"><?php echo esc_html($link_text); ?></a>
+                </p>
+            <?php endif; ?>
 
         </section>
 
@@ -239,18 +150,18 @@
         // 現在の投稿のタームを取得
         $terms = get_the_terms(get_the_ID(), 'facility_type');
         $term_slug = '';
-
         if ($terms && !is_wp_error($terms)) {
             // 最初のタームのスラッグを取得
             $term_slug = $terms[0]->slug;
         }
         ?>
 
-        <button class="back_btn" onclick="window.location.href='<?php echo home_url('/column_type/' . $term_slug . '/'); ?>'"><span><i class="fa-solid fa-arrow-left"></i>back</span></button>
+        <button class="back_btn" onclick="window.location.href='<?php echo home_url('/column_type/' . $term_slug . '/'); ?>'">
+            <span><i class="fa-solid fa-arrow-left"></i>back</span>
+        </button>
 
-
+    </div>
 </main>
-</div>
 
-<!-- footer.phpを読み込む次の記事
+<!-- footer.phpを読み込む -->
 <?php get_footer(); ?>
