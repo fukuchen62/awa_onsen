38206e328d485bcf70a935f26686b7521870fe83
keisukematsuka <matsukakeisuke0202@gmail.com>
2024-06-14 13:32:06 +0900

    新着ニュース更新
    ページネーション更新

--- category.php
+++ category.php
@@ -46,9 +46,7 @@ get_header();
                     $args = array(
                         'category_name' => $current_category->slug, // 現在のカテゴリーのスラッグ
                         'posts_per_page' => 5, // 表示する投稿数
-                        'paged' => get_query_var('paged') ? get_query_var('paged') : 1 // ページネーション
                     );
-
                     // WP_Queryオブジェクトを作成
                     $category_query = new WP_Query($args);
 
