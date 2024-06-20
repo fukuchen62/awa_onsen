4cf97bc7fadcf77313db0c0618fc6552dfcd17e9
sasatoko <emthisa31.26@gmail.com>
2024-06-20 16:52:34 +0900

    Update form.js
    お問合せ初期化処理js
    江本

--- assets/js/form.js
+++ assets/js/form.js
@@ -19,8 +19,13 @@
 //     });
 //
 $(function () {
-    let contact_chec = document.getElementsByClassName('parsonal_imfomation_checkbox_data');
+    let contact_chec = document.getElementsByClassName
+        ('parsonal_imfomation_checkbox_data');
     let contact_sbmit = document.getElementsByClassName('contact_form_btns_sbmit');
+    // 初期状態
+    $(contact_sbmit).css('pointer-events', 'none');
+    $(contact_sbmit).css('background', '#D9D9D9');
+    // クリックアクション
     $(contact_chec).on('click', function () {
         if ($(this).prop("checked") == true) {
             $(contact_sbmit).css('pointer-events', 'auto');
