a862fc0c6562aaf25fe6818819a1ff4d1a250c47
UsagiHituzi <rabbit.brown.maronjij@gmail.com>
2024-07-01 11:32:12 +0900

    更新しました
    ボタン、文字更新しました

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,23 +1,5 @@
 "use strict";
 
-//
-//     // チェックボックスの要素を取得
-//     const $agreeCheckbox = $("#privacy");
-//     // 送信ボタンの要素を取得
-//     const $submitBtn = $("#submit_button");
-
-//     // チェックボックスが変更されたとき
-//     $agreeCheckbox.on("change", function () {
-//         // チェックされている場合
-//         if ($(this).prop("checked")) {
-//             // 送信ボタンを有効にする
-//             $submitBtn.prop("disabled", false);
-//         } else {
-//             // 送信ボタンを無効にする
-//             $submitBtn.prop("disabled", true);
-//         }
-//     });
-//
 $(function () {
     let contact_chec = document.getElementsByClassName
         ('parsonal_imfomation_checkbox_data');
@@ -29,7 +11,7 @@ $(function () {
     $(contact_chec).on('click', function () {
         if ($(this).prop("checked") == true) {
             $(contact_sbmit).css('pointer-events', 'auto');
-            $(contact_sbmit).css('background', '#FBD266');
+            $(contact_sbmit).css('background', '#538fcb');
         } else {
             $(contact_sbmit).css('pointer-events', 'none');
             $(contact_sbmit).css('background', '#D9D9D9');
