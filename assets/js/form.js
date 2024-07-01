d2937e5b14763fb440d04f7a16ae71b5c5312eab
UsagiHituzi <rabbit.brown.maronjij@gmail.com>
2024-07-01 12:17:45 +0900

    色変更

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,29 +1,5 @@
 "use strict";
 
-// $(function () {
-//     let contact_chec = document.getElementsByClassName
-//         ('parsonal_imfomation_checkbox_data');
-//     let contact_sbmit = document.getElementsByClassName('contact_form_btns_sbmit');
-//     // 初期状態
-//     $(contact_sbmit).css('pointer-events', 'none');
-//     $(contact_sbmit).css('background', '#D9D9D9');
-//     // クリックアクション
-//     $(contact_chec).on('click', function () {
-//         if ($(this).prop("checked") == true) {
-//             $(contact_sbmit).css('pointer-events', 'auto');
-//             $(contact_sbmit).css('background', '#538fcb');
-//         } else {
-//             $(contact_sbmit).css('pointer-events', 'none');
-//             $(contact_sbmit).css('background', '#D9D9D9');
-//         }
-
-//     });
-
-
-// });
-
-
-
 $(function () {
     // 要素の取得
     let contact_chec = $(".parsonal_imfomation_checkbox_data");
@@ -39,8 +15,8 @@ $(function () {
     $(contact_chec).on("click", function () {
         if ($(this).prop("checked")) {
             $(contact_sbmit).css("pointer-events", "auto");
-            $(contact_sbmit).css("background", "#538fcb");
-            $(contact_sbmit).css("border", "2px solid #538fcb");
+            $(contact_sbmit).css("background", "#f5a200");
+            $(contact_sbmit).css("border", "2px solid #f5a200");
 
         } else {
             $(contact_sbmit).css("pointer-events", "none");
@@ -55,8 +31,8 @@ $(function () {
         function () {
             // ホバー時に背景色を白に変更
             $(this).css("background", "#fff");
-            $(this).css("color", "#538fcb");
-            $(this).css("border", "2px solid #538fcb");
+            $(this).css("color", "#f5a200");
+            $(this).css("border", "2px solid #f5a200");
             $(this).css("transition", "all 0.4s");
 
 
@@ -64,7 +40,7 @@ $(function () {
         function () {
             // ホバー終了時に背景色を元に戻す
             if ($(contact_chec).prop("checked")) {
-                $(contact_sbmit).css("background", "#538fcb");
+                $(contact_sbmit).css("background", "#f5a200");
                 $(this).css("color", "#fff");
 
 
